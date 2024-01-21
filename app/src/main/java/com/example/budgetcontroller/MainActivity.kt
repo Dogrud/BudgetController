19f3b91943698b7cfbd3b7286362c16542e81d99
dogukan <dogukandogru06@gmail.com>
2024-01-21 11:06:56 +0300

    setting up the theme to dark mode

--- app/src/main/java/com/example/budgetcontroller/MainActivity.kt
+++ app/src/main/java/com/example/budgetcontroller/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.budgetcontroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import com.example.budgetcontroller.ui.theme.BudgetControllerTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +36,9 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        color = Color.Black,
+        modifier = Modifier
+            .background(Color.White)
     )
 }
 
