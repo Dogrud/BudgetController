a9c51febfafaa8e05d0484b2c63e71a029b20caf
dogukan <dogukandogru06@gmail.com>
2024-01-24 23:47:49 +0300

    edited the design of the settings page

--- app/src/main/java/com/example/budgetcontroller/components/TableRow.kt
+++ app/src/main/java/com/example/budgetcontroller/components/TableRow.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -23,10 +25,12 @@ import com.example.budgetcontroller.ui.theme.TextPrimary
 fun TableRow(label: String, hasArrow: Boolean = false ,isDestructive:Boolean = false) {
     val textColor = if(isDestructive == true) Destructive else TextPrimary
 
-Row(modifier   = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+Row(modifier   = Modifier
+    .fillMaxWidth()
+    .padding(horizontal = 16.dp, vertical = 10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
     Text(text = label, style = TextStyle(fontFamily = FontFamily.Default, color = textColor))
     if(hasArrow) {
-       Icon(painterResource(id = R.drawable.baseline_arrow_forward_24),contentDescription = "Right Arrow" )
+       Icon(painterResource(id = R.drawable.baseline_arrow_forward_24),contentDescription = "Right Arrow")
     }
 }
 
--- app/src/main/java/com/example/budgetcontroller/pages/Settings.kt
+++ app/src/main/java/com/example/budgetcontroller/pages/Settings.kt
@@ -1,16 +1,25 @@
 package com.example.budgetcontroller.pages
 
 import android.provider.Settings
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.budgetcontroller.components.TableRow
+import com.example.budgetcontroller.ui.theme.BackgroundElevated
+import com.example.budgetcontroller.ui.theme.DividerColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,8 +29,16 @@ fun Settings(navController: NavController){
         content = {innerPadding ->
             Column (modifier = Modifier.padding(innerPadding))
             {
-                TableRow(label = "Categories", hasArrow = true)
-                TableRow(label = "Erase all data", isDestructive = true)
+                Column (modifier = Modifier
+                    .padding(16.dp)
+                    .background(BackgroundElevated)
+                    .fillMaxWidth()
+                ){
+                    TableRow(label = "Categories", hasArrow = true)
+                    Divider(modifier = Modifier.padding(start = 16.dp), thickness = 1.dp, color = DividerColor)
+                    TableRow(label = "Erase all data", isDestructive = true)
+                }
+
             }
         },
         topBar = {
